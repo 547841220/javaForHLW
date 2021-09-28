c6de539df2fb08e4c8e54656e447ab15b30a2bf5
547841220 <18935114910@163.com>
2021-09-28 18:57:46 +0800

    接口和抽象类

---
+++ src/interfaceAndabstract/AbstractTest.java
@@ -0,0 +1,16 @@
+package interfaceAndabstract;
+
+/**
+ * Description: AbstractTest
+ *
+ * @author jijie
+ * @date 2021/9/28 @time 18:51
+ */
+public abstract class AbstractTest {
+
+    public abstract void fun1();
+
+    public void fun2(){
+        System.out.println("fun2");
+    }
+}
---
+++ src/interfaceAndabstract/InterfaceTest.java
@@ -0,0 +1,15 @@
+package interfaceAndabstract;
+
+/**
+ * Description: InterfaceTest
+ *
+ * @author jijie
+ * @date 2021/9/28 @time 18:51
+ */
+public interface InterfaceTest {
+    //接口里面的字段默认static final类型
+    public static final String s = "123";
+    public abstract void fun1();
+    public abstract void fun2();
+
+}
